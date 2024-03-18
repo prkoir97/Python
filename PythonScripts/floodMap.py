c4db6b579140aa207981cb82f8a893e8dfb5d55d
Priya <56896239+prkoir97@users.noreply.github.com>
2024-03-18 13:52:30 -0400

    Update floodMap.py

--- PythonScripts/floodMap.py
+++ PythonScripts/floodMap.py
@@ -6,20 +6,22 @@ import matplotlib.pyplot as plt
 
 elevations = np.loadtxt('elevationsNYC.txt')
 
-mapShape = elevations.shape + (3,)
+# Create an empty flood map with the same shape as the elevation data
+mapShape = elevations.shape + (3,)            # Add additional dimension for color channels (RGB)
 floodMap = np.zeros(mapShape)
 
 for row in range(mapShape[0]):
-    for col in range(mapShape[1]):
-        if elevations[row, col] <= 0:
-
+    for col in range(mapShape[1]):               # Check elevation levels and assign colors accordingly
+        if elevations[row, col] <= 0:            # Below sea level, set to blue for water
             floodMap[row, col, 2] = 1.0
-        elif elevations[row, col] <= 6:
+
+        elif elevations[row, col] <= 6:          # Between 0 and 6 feet, set to red for potential flood risk
             floodMap[row, col, 0] = 1.0
 
-        elif elevations[row, col] <= 20:  # less than 20ft set to gray at 50%
+        elif elevations[row, col] <= 20:         # Between 6 and 20 feet, set to gray at 50% for lower risk areas
             floodMap[row, col, :] = 0.5
-        else:
+
+        else:                                    # Above 20 feet, set to green for higher ground
             floodMap[row, col, 1] = 1.0
 
 plt.imsave('floodMap.png', floodMap)
